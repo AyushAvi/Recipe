845917a49ae4a16841c950e7ac582e2fe93bda5d
AyushAvi <70689163+AyushAvi@users.noreply.github.com>
2022-04-06 01:21:24 +0530

    Completed

---
+++ script/script.js
@@ -0,0 +1,8 @@
+function nav() {
+    var x = document.getElementById('navBarmob');
+    if (x.style.display == "none"){
+        x.style.display = "flex";
+    } else{
+        x.style.display = "none";
+    }
+}
